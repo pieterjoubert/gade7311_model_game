0e9dcc1cc33fd6e7a350a66ab7fc3d459e10215b
pieterjoubert <pieter.joubert@gmail.com>
2020-05-07 12:40:05 +0200

    Initial Commit. Prefabs, materials and Map Script.

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public GameObject redWall;
+    public GameObject blueWall;
+    public GameObject redCube;
+    public GameObject blueCube;
+    public GameObject peg;
+    public GameObject emptyWall;
+
+    public int height;
+    public int width;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*for (int z = 0; z < height; z++)
+        {
+            for (int x = 0; x < width; x++)
+            {*/
+                Instantiate(peg, new Vector3((float) x, 0f, (float)z), Quaternion.identity);
+         /*   }
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
